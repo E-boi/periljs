42bfe476c66dbef1e3e5ffb769e6da995ea04716
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-08-04 22:35:51 -0400

    constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,15 @@
+import { Opcodes, Closecodes, JSONcodes, Voicecodes } from "./ent/intf/discord/opcodes";
+import { Permissions } from "./ent/intf/discord/permissions";
+import { Intents } from "./ent/intf/discord/intents";
+import { UserFlags } from "./ent/intf/discord/user/flags";
+
+export default {
+    Opcodes,
+    Closecodes,
+    JSONcodes,
+    Voicecodes,
+    Permissions,
+    Intents,
+    UserFlags
+};
+        
