835de9c69b65ccf18347b92da02944dc69219de6
pog who? <54505527+E-boi@users.noreply.github.com>
2021-10-14 16:59:21 -0400

    s

--- ex/more.ts
+++ ex/more.ts
@@ -6,7 +6,7 @@ import { Client, Intents } from '@e-boi/periljs';
 const client = new Client({ token: 'a token here', intents: [Intents.GUILDS] });
 
 client.on('ready', () => {
-	client.command.set({ type: 'CHAT_INPUT', name: 'example', description: 'example slash command made with periljs' }, interaction => {
+	client.commands.set({ type: 'CHAT_INPUT', name: 'example', description: 'example slash command made with periljs' }, interaction => {
 		interaction.reply({
 			content: 'Click a button below!',
 			components: [
