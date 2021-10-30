bfdfabd20637063474c3923bdf46d041abcc5984
Jai <jailovehart@gmail.com>
2021-10-29 20:15:13 -0400

    swapping to vsc i dislike ghdev

---
+++ lib/ent/impl/Embed.ts
@@ -0,0 +1,13 @@
+import {
+    IEmbed,
+    IEmbedAuthor,
+    IEmbedField,
+    IEmbedFooter,
+    IEmbedImage,
+    IEmbedProvider
+} from "../intf/IEmbed";
+
+export default class Embed implements IEmbed
+{
+
+}
